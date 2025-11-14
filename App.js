1d21ec214e5934760e0000ea937f3973aa0b3aff
Damitimo <toojo@usc.edu>
2025-11-14 10:30:05 -0800

    Initial commit for PillPerk mobile app

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Welcome to PI lPerk!</Text>
+      <Button title="Get Started" onPress={() => alert('Hello!')} />
       <StatusBar style="auto" />
     </View>
   );
